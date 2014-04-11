9bafc4c4ea5b35f38c44d122afd914fb5e8f25c1
Jeff Kowalski <jeff.kowalski@gmail.com>
2014-04-11 00:14:12 -0700

    Address flycheck warnings; remove ac detritus

--- personal/custom.el
+++ personal/custom.el
@@ -1,11 +1,13 @@
+;;; custom.el -- emacs customization file
+;;; Commentary:
+;;; Code:
+;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-modes
-   (quote
-    (emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode org-mode)))
  '(ad-redefinition-action (quote accept))
  '(ahk-syntax-directory "c:/Program Files (x86)/AutoHotkey/Extras/Editors/Syntax/")
  '(ansi-color-names-vector
@@ -74,5 +76,5 @@
  '(org-warning ((t (:foreground "#cc9393" :weight bold))))
  '(region ((t (:background "dim gray")))))
 
-(provide '.emacs-custom)
-;;; .emacs-custom.el ends here
+(provide 'custom)
+;;; custom.el ends here
