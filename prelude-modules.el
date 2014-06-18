2fb3f6ebd9a9f9bab97ad5686612fdcc19ebdea4
Jeff Kowalski <jeff.kowalski@gmail.com>
2014-06-18 09:51:48 -0700

    Enable key chord and python prelude modules.

--- prelude-modules.el
+++ prelude-modules.el
@@ -14,13 +14,13 @@
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
-;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
 ;; (require 'prelude-mediawiki)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-;; (require 'prelude-python)
+(require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
