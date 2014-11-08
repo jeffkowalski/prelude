7fa5f02280d1f4479f20ec8143fde93a2e551a6d
Jeff Kowalski <jeff.kowalski@gmail.com>
2014-11-07 20:19:48 -0800

    Prefer helm everywhere

--- prelude-modules.el
+++ prelude-modules.el
@@ -1,18 +1,18 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-(require 'prelude-helm) ;; Interface for narrowing and search
-(require 'prelude-company)
-
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
+(require 'prelude-company)
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 (require 'prelude-erc) ;; Emacs IRC client
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-haskell)
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-js)
 (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-latex)
@@ -21,7 +21,7 @@
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 (require 'prelude-python)
-;; (require 'prelude-ruby)
+(require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
