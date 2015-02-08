be03633ef87980b518ed69b07d52549064526bc9
Jeff Kowalski <jeff.kowalski@gmail.com>
2015-02-08 00:38:58 -0800

    Workaround python startup issue; remove older workarounds

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -15,14 +15,15 @@
 
 ;; FIXME: defines to overcome warning about reference to
 ;; free variables in packages
-;;     packages that use ido...
-(defvar inherit-input-method nil)
-(defvar ido-cur-item nil)
-(defvar ido-default-item nil)
-(defvar ido-cur-list nil)
-(defvar ido-context-switch-command nil)
 ;;     helm-swoop...
 (defvar helm-swoop-last-prefix-number nil)
+
+;; FIXME: define-abbrev to workaround startup error
+;;   Compiler-macro error for python-syntax-context: (void-function python-syntax--context-compiler-macro) [2 times]
+;;   /home/jeff/.emacs.d/elpa/expand-region-20141223.1328/python-el-fgallina-expansions.el
+;;   http://lists.gnu.org/archive/html/emacs-diffs/2013-04/msg00113.html
+(unless (fboundp 'python-syntax--context-compiler-macro)
+  (defun python-syntax--context-compiler-macro (a b c) nil))
 
 
 (provide 'preload)
