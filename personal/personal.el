979d313d6724c60fda32ae4000b58b8ca004fe9a
Jeff Kowalski <jeff.kowalski@gmail.com>
2016-02-03 23:14:32 -0800

    Add org-expiry to insert CREATED todo property

--- personal/personal.el
+++ personal/personal.el
@@ -39,6 +39,12 @@
 (progn
   (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
   (setq el-get-sources '(
+                         (:name org-expiry
+                                :description "Expiry mechanism for Org entries"
+                                :type http
+                                :url "http://orgmode.org/w/?p=org-mode.git;a=blob_plain;f=contrib/lisp/org-expiry.el;hb=HEAD"
+                                :localname "org-expiry.el"
+                                :features org-expiry)
                          (:name evernote-mode
                                 :description "Functions for editing Evernote notes directly from Emacs"
                                 :type github
@@ -953,6 +959,57 @@ be global."
   :loader el-get-local
   :require (cua-base org)
   :init (org-cua-dwim-activate))
+
+;; org expiry
+
+(req-package org-expiry
+  :loader el-get-local
+  :require org-capture
+  :init (progn
+          (org-expiry-insinuate)
+          ;;
+          ;; Allow automatically handing of created/expired meta data.
+          ;;
+          ;; Configure it a bit to my liking
+          (setq
+           org-expiry-created-property-name "CREATED" ; Name of property when an item is created
+           org-expiry-inactive-timestamps   t         ; Don't have everything in the agenda view
+           )
+
+          (defun mrb/insert-created-timestamp()
+            "Insert a CREATED property using org-expiry.el for TODO entries"
+            (org-expiry-insert-created)
+            (org-back-to-heading)
+            (org-end-of-line)
+            (insert " ")
+            )
+
+          ;; Whenever a TODO entry is created, I want a timestamp
+          ;; Advice org-insert-todo-heading to insert a created timestamp using org-expiry
+          (defadvice org-insert-todo-heading (after mrb/created-timestamp-advice activate)
+            "Insert a CREATED property using org-expiry.el for TODO entries"
+            (mrb/insert-created-timestamp)
+            )
+          ;; Make it active
+          (ad-activate 'org-insert-todo-heading)
+
+          (defadvice org-capture (after mrb/created-timestamp-advice activate)
+            "Insert a CREATED property using org-expiry.el for TODO entries"
+                                        ; Test if the captured entry is a TODO, if so insert the created
+                                        ; timestamp property, otherwise ignore
+            (when (member (org-get-todo-state) org-todo-keywords-1)
+              (mrb/insert-created-timestamp)))
+          (ad-activate 'org-capture)
+
+          ;; Add feature to allow easy adding of tags in a capture window
+          (defun mrb/add-tags-in-capture()
+            (interactive)
+            "Insert tags in a capture window without losing the point"
+            (save-excursion
+              (org-back-to-heading)
+              (org-set-tags)))
+          ;; Bind this to a reasonable key
+          (define-key org-capture-mode-map "\C-c\C-t" 'mrb/add-tags-in-capture)))
 
 ;; ----------------------------------------------------------- [ org-ehtml ]
 
